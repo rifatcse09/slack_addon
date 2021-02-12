b4c7d5bf2eeeda472962d5be37e57f04f83456f7
rifatcse09 <rifatcse09@gmail.com>
2021-02-12 12:29:11 +0600

    token verify

--- public/js/auth.js
+++ public/js/auth.js
@@ -49,7 +49,7 @@ $(document).ready(function(){
 
     console.log(data);
 
-    $.post('/tokenverify', data, function(response) {
+    $.post('/slack/tokenverify', data, function(response) {
         console.log(response.message);
         if(response.message != 'error'){
         window.location.href = response.message;
